f77aaff071d7de160117e5cac75fd5303f2d1f4b
Josselin <julien.josselin75@hotmail.fr>
2021-09-22 23:12:34 +0200

    hit space/enter to start game

--- scripts/UI.gd
+++ scripts/UI.gd
@@ -1,21 +1,30 @@
 extends Control
 
 signal start_game
+enum {
+	GAMEOVER
+	MENU
+	PLAYING
+}
+var state = MENU
 var time_score = 0
 
 
 func show_screen(name):
 	match name:
 		"GameOver":
+			state = GAMEOVER
 			$HUD/ScoreTimer.stop()
 			$HUD/ScoreBackground.hide()
 			$GameOver.show()
 			yield(get_tree().create_timer(1.0), "timeout")
 		"Menu":
+			state = MENU
 			$GameOver.hide()
 			$HUD/ScoreBackground.hide()
 			$Menu.show()
 		"Game":
+			state = PLAYING
 			$HUD/ScoreTimer.start()
 			$HUD/ScoreBackground.show()
 			$GameOver.hide()
@@ -30,8 +39,12 @@ func _on_Play_pressed():
 
 func _unhandled_input(event):
 	if event.is_action_pressed("ui_accept"):
-		show_screen("Menu")
-		get_tree().reload_current_scene()
+		if state == GAMEOVER:
+			show_screen("Menu")
+			get_tree().reload_current_scene()
+		elif state == MENU:
+			show_screen("Game")
+			_on_Play_pressed()
 
 func _on_ScoreTimer_timeout():
 	time_score += 1
